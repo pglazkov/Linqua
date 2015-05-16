e6ac5e6d11c6bf00b97806c15ba7fd36011a25e7
Pavlo Glazkov <p.glazkov@gmail.com>
2015-05-16 11:17:52 +0200

    Disabled XamlSpy. Implemented "displaying busy status after 0.5 seconds".

---
+++ Linqua.WindowsPhone/Framework/BusyStatus.cs
@@ -0,0 +1,18 @@
+﻿using Framework;
+using JetBrains.Annotations;
+
+namespace Linqua.Framework
+{
+	public class BusyStatus
+	{
+		public BusyStatus([NotNull] string statusText)
+		{
+			Guard.NotNullOrEmpty(statusText, () => statusText);
+
+			StatusText = statusText;
+		}
+
+		[NotNull]
+		public string StatusText { get; private set; }
+	}
+}
--- Linqua.WindowsPhone/Framework/StatusBusyService.cs
+++ Linqua.WindowsPhone/Framework/StatusBusyService.cs
@@ -2,7 +2,7 @@
 using System.Composition;
 using System.Reactive.Disposables;
 using Windows.UI.ViewManagement;
-using Framework;
+using Windows.UI.Xaml;
 using Framework.PlatformServices;
 
 namespace Linqua.Framework
@@ -10,19 +10,58 @@ namespace Linqua.Framework
 	[Export(typeof(IStatusBusyService))]
 	public class StatusBusyService : IStatusBusyService
 	{
-		[Import]
-		public IDispatcherService Dispatcher { get; set; }
+		private static readonly TimeSpan DefaultDisplayAfter = TimeSpan.FromSeconds(0.5);
+
+		private readonly DispatcherTimer displayAfterTimer = new DispatcherTimer();
+
+		private BusyStatus currentBusyStatus;
+
+		public StatusBusyService()
+		{
+			displayAfterTimer.Interval = DefaultDisplayAfter;
+			displayAfterTimer.Tick += OnDisplayAfterTimerTick;
+		}
 
 		public IDisposable Busy(string statusText)
 		{
-			StatusBarProgressIndicator progressbar = StatusBar.GetForCurrentView().ProgressIndicator;
-			progressbar.Text = statusText ?? "Loading...";
-			var _ = progressbar.ShowAsync();
+			displayAfterTimer.Stop();
+
+			currentBusyStatus = new BusyStatus(statusText ?? "Loading...");
+
+			displayAfterTimer.Start();
 
 			return Disposable.Create(() =>
 			{
-				_ = progressbar.HideAsync();
+				HideStatus(currentBusyStatus);
 			});
+		}
+
+		private void OnDisplayAfterTimerTick(object sender, object e)
+		{
+			var s = currentBusyStatus;
+
+			if (s != null)
+			{
+				ShowStatus(s);
+			}
+		}
+
+		private void ShowStatus(BusyStatus status)
+		{
+			var progressbar = StatusBar.GetForCurrentView().ProgressIndicator;
+			progressbar.Text = status.StatusText;
+			var _ = progressbar.ShowAsync();
+		}
+
+		private void HideStatus(BusyStatus status)
+		{
+			displayAfterTimer.Stop();
+
+			currentBusyStatus = null;
+
+			var progressbar = StatusBar.GetForCurrentView().ProgressIndicator;
+
+			var _ = progressbar.HideAsync();
 		}
 	}
 }
