04a633b779ba89681c4714ce1f3cf0557053e4c3
Pavlo Glazkov <p.glazkov@gmail.com>
2015-06-12 07:51:04 +0200

    Displaying learned entries with a little opacity in order to more easily distinguish them from the not learned once.

---
+++ Framework.WinRT/Converters/BooleanToDoubleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Framework.Converters
+{
+	public class BooleanToDoubleConverter : IValueConverter
+	{
+		public double TrueValue { get; set; }
+		public double FalseValue { get; set; }
+		public double NullValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (ReferenceEquals(value, null))
+			{
+				return NullValue;
+			}
+
+			var boolValue = (bool)value;
+
+			return boolValue ? TrueValue : FalseValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
