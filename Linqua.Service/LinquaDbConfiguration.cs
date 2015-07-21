8e6d4daa1bea69517f2d4588c1636d307278872e
Pavlo Glazkov <p.glazkov@gmail.com>
2015-07-21 07:33:50 +0200

    Added a retry policy for possible transient errors in SQL Azure. Removed local DB files from the version control.

---
+++ Linqua.Service/LinquaDbConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace Linqua.Service
+{
+    public class LinquaDbConfiguration : DbConfiguration
+    {
+        public LinquaDbConfiguration()
+        {
+            // See https://msdn.microsoft.com/en-us/data/dn456835.aspx?f=255&MSPPError=-2147217396
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
