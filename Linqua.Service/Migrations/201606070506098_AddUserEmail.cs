5953a69b25e76996a736baa607df22ff3308cc4f
Pavlo Glazkov <p.glazkov@gmail.com>
2016-06-07 07:43:27 +0200

    Added UserEmail column.

---
+++ Linqua.Service/Migrations/201606070506098_AddUserEmail.cs
@@ -0,0 +1,18 @@
+namespace Linqua.Service.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserEmail : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("linqua.Entries", "UserEmail", c => c.String(maxLength: 256));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("linqua.Entries", "UserEmail");
+        }
+    }
+}
